e417768dd3cc2d067509f21e0c31b95a4f797446
LinZ-debug <zhaochy1@Shanghaitech.edu.cn>
2020-10-19 08:18:19 +0000

    update designdoc

--- userprog/process.c
+++ userprog/process.c
@@ -101,7 +101,7 @@ process_exit (void)
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
-  if (pd != NULL) 
+  if (pd != NULL)
     {
       /* Correct ordering here is crucial.  We must set
          cur->pagedir to NULL before switching page directories,
